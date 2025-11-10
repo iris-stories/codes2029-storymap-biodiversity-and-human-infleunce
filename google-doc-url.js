e65087a465b6eb0355a219ce9e778c7d43619f53
danschreiberSIUE <daschre@siue.edu>
2025-11-10 14:21:19 -0600

    Add Google Sheets API key and URL

--- google-doc-url.js
+++ google-doc-url.js
@@ -3,3 +3,4 @@ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1jiczwj4nOdLB_yfxeYDK
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+
